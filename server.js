f9672082c3642e3e071cd092cfee80e59aeadd99
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-11-02 19:30:55 +0000

    Added own url address from glitch

--- server.js
+++ server.js
@@ -21,3 +21,4 @@ app.get("/bookings", function (request, response) {
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
