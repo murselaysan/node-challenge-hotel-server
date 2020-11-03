19b3fe90a88a1a2fd1f007dc400e2950619dee11
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-11-03 19:32:35 +0000

    LEVEL 2 DONE

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+var moment = require("moment");
+moment().format();
+
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
@@ -19,63 +22,69 @@ app.get("/bookings", function (request, response) {
 });
 
 //Read one booking, specified by an ID
-app.get('/bookings/:id', function(req, res) {
-  if(isNaN( req.params.id)){
+app.get("/bookings/:id", function (req, res) {
+  if (isNaN(req.params.id)) {
     res.send({
-        "message": "undefined message"
-    })
-  }else{
- const message = bookings.find(u => u.id==req.params.id)
- if(message){
-     res.send(message)
- }else{
-     res.send({
-         "message" : "Data Did Not Find"
-     })
- }
+      message: "undefined message",
+    });
+  } else {
+    const message = bookings.find((u) => u.id == req.params.id);
+    if (message) {
+      res.send(message);
+    } else {
+      res.send({
+        message: "Data Did Not Find",
+      });
+    }
   }
 });
 
 /// Delete user with ID//
-app.delete('/bookings/:id', function(request, response) {
-  let messageId = Number(request.params.id)
-  let index = bookings.findIndex(message=> message.id === messageId)
-  let deleted = bookings.splice(index, 1)
+app.delete("/bookings/:id", function (request, response) {
+  let messageId = Number(request.params.id);
+  let index = bookings.findIndex((message) => message.id === messageId);
+  let deleted = bookings.splice(index, 1);
   response.json(bookings);
 });
 
 //create new  booking
 app.post("/bookings", (req, res) => {
-  let newId = bookings[bookings.length -1].id + 1
-  let newRoomId = bookings[bookings.length -1].id + 1
-  let newBooking= {
-    id:req.body.newId,
-    title,
-    firstName,
-    surname,
-    email,
-    roomId:req.body.newRoomId,
-    checkInDate,
-    checkOutDate
-  } = req.body;
+  let newId = new Date().getTime();
+  let newRoomId = bookings[bookings.length - 1].id + 1;
+
+  let newBooking = {
+    id: newId,
+    title: req.body.title,
+    firstName: req.body.firstName,
+    surname: req.body.surname,
+    email: req.body.email,
+    roomId: newRoomId,
+    checkInDate: req.body.checkInDate,
+    checkOutDate: req.body.checkOutDate,
+  };
   if (
-    !newBooking.id ||
-    !newBooking.title ||
-    !newBooking.firstName ||
-    !newBooking.surname ||
-    !newBooking.email ||
-    !newBooking.roomId ||
-    !newBooking.checkInDate ||
-    !newBooking.checkOutDate
-  )
-    return res.status(400).send("Please Enter All data");
-
-  bookings.push(newBooking);
-  res.json(bookings);
-  });
-
+    newBooking.checkInDate !== undefined &&
+    newBooking.checkInDate.length > 0 &&
+    newBooking.checkOutDate !== undefined &&
+    newBooking.checkOutDate.length > 0 &&
+    newBooking.title !== undefined &&
+    newBooking.title.length > 0 &&
+    newBooking.firstName !== undefined &&
+    newBooking.firstName.length > 0 &&
+    newBooking.surname !== undefined &&
+    newBooking.surname.length > 0 &&
+    newBooking.email !== undefined &&
+    newBooking.email.includes("@") &&
+    moment(newBooking.checkOutDate).isAfter(newBooking.checkInDate)
+  ) {
+    bookings.push(newBooking);
+    // res.send("added")
+    res.send("added");
+  } else {
+    res.send(" 400,bad request");
+  }
+});
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
